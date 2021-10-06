4571d361139391933539ee5aad9820da402a8687
Rom0105 <savosko.photo@gmail.com>
2021-10-06 15:56:26 +0300

    правки

---
+++ src/components/App/App.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Section from '../Section/Section';
+import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
+import Statistics from '../Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = event => {
+    const label = event.target.textContent;
+
+    this.setState(prevState => ({ [label]: (prevState[label] += 1) }));
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.keys(this.state).reduce((acc, value) => acc + this.state[value], 0);
+
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const percent = Math.round((this.state.good * 100) / this.countTotalFeedback());
+
+    return percent;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={this.state}
+           onLeaveFeedback={this.onLeaveFeedback} />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
